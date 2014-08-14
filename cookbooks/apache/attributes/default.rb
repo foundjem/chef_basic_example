5e51ad983bf7f6958ea82e892cc3197c5542b743
sandro brunner <brunnersandro@hotmail.com>
2014-08-14 17:50:02 +0200

    changes in apache cookbook

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,18 +1,10 @@
 case node["platform_family"]
 when "debian"
-  default["package_name"] = "apache2"
-  default["service_name"] = "apache2"
-  default["document_root"] = "/var/www"
+  default["apache"]["package_name"] = "apache2"
+  default["apache"]["service_name"] = "apache2"
+  default["apache"]["document_root"] = "/var/www"
 when "rhel"
-  default["package_name"] = "httpd"
-  default["service_name"] = "httpd"
-  default["document_root"] = "/var/www/html"
+  default["apache"]["package_name"] = "httpd"
+  default["apache"]["service_name"] = "httpd"
+  default["apache"]["document_root"] = "/var/www/html"
 end
-
-
-
-
-
-
-
-
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -15,15 +15,15 @@
 # 3. Make sure that the service starts on reboot
 # 4. Write out home page
 
-package node["package_name"] do
+package node["apache"]["package_name"] do
 	action :install 
 end
 
-service node["service_name"] do
+service node["apache"]["service_name"] do
 	action [:start, :enable]
 end
 
-template "#{node["document_root"]}/index.html" do
+template "#{node["apache"]["document_root"]}/index.html" do
   source "index.html.erb"
   mode "0644"
 end

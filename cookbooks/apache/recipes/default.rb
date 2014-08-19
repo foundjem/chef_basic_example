42c3a8fb9e3d10b4abb9a3ec09efc14e1920e23a
sandro brunner <brunnersandro@hotmail.com>
2014-08-19 14:06:22 +0200

    adapted default.rb recipe

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -17,15 +17,10 @@ end
 
 remote_directory "#{node["apache"]["document_root"]}" do
   source "example_page"
-  mode = "0770"
-  owner = "un_folder_owner"
-  files_mode = "0440"
-  files_owner = "un_files_owner"
 end
 
 template "#{node["apache"]["document_root"]}/index.html" do
   source "index.html.erb"
-  mode "0644"
   variables({
     :username => "icclab",
     :password => "hallo welt",
